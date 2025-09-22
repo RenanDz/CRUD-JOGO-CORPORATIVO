70b8eac231e9bd4b49cbc53693f122cb7e2b256a
Renan Dias <renandias01@yahoo.com.br>
2025-09-22 09:13:05 -0300

    index.php
    Menu principal do CRUD

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Jogo Corporativo - CRUD</title>
+</head>
+<body>
+    <h1>Menu Principal</h1>
+    <ul>
+        <li><a href="usuarios.php">Interface Usuario</a></li>
+        <li><a href="criar.php">Criar Pergunta</a></li>
+        <li><a href="listar.php">Listar Todas Perguntas</a></li>
+        <li><a href="ver.php">Ver Pergunta</a></li>
+        <li><a href="alterar.php">Alterar Pergunta</a></li>
+        <li><a href="excluir.php">Excluir Pergunta</a></li>
+    </ul>
+</body>
+</html>
