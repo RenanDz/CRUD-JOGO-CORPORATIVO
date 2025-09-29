7972ae8ecf9ad10a634f8cad9bbaa8eabf2683c7
Renan Dias <renandias01@yahoo.com.br>
2025-09-29 08:32:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 <head><title>Game Corporativo</title></head>
 <body>
 <h1>Menu</h1>
+
+<!-- Usuários -->
 <a href="usuario.php?listar=1">Listar Usuários</a><br>
-<a href="pergunta.php?listar=1">Listar Perguntas</a><br>
 <form method="post" action="usuario.php">
     <h3>Criar Usuário</h3>
     Nome: <input type="text" name="nome"><br>
@@ -13,20 +14,25 @@
     <input type="hidden" name="acao" value="criar">
     <button type="submit">Salvar</button>
 </form>
+<hr>
+
+<!-- Perguntas -->
+<a href="pergunta.php?listar=1">Listar Perguntas</a><br>
 <form method="post" action="pergunta.php">
     <h3>Criar Pergunta</h3>
     Texto: <input type="text" name="texto"><br>
-    Tipo: 
+    Tipo:
     <select name="tipo">
         <option value="texto">Texto</option>
         <option value="multipla">Múltipla escolha</option>
     </select><br>
-    Respostas: <br>
-    <input type="text" name="respostas[]"> <br>
-    <input type="text" name="respostas[]"> <br>
-    Correta (índice): <input type="number" name="correta"><br>
+    <p>Respostas (somente se múltipla escolha):</p>
+    <input type="text" name="respostas[]"><br>
+    <input type="text" name="respostas[]"><br>
+    <input type="text" name="respostas[]"><br>
     <input type="hidden" name="acao" value="criar">
     <button type="submit">Salvar</button>
 </form>
+
 </body>
 </html>
